21e2e772713102060cb10cdd4ee4789d9c251ed1
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2025-09-19 18:37:52 -0400

    Update menuV2.1.0.js

--- menuV2.1.0.js
+++ menuV2.1.0.js
@@ -734,7 +734,7 @@ winBtn.close:hover {
             }
         ],
         'Tools': [{
-                text: 'Cloaker',
+                text: 'Tab Cloaker',
                 description: 'Disguise tab',
                 url: 'https://cdn.jsdelivr.net/gh/CidCaribou/Executor-Scripts@refs/heads/main/Tab_Cloaker.js'
             },
